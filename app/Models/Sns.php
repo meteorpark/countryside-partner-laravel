a9fc9ca2cd7db9ea80628a5a03ace3d4d1c6e145
rzip84 <rzip84@naver.com>
2019-06-28 14:53:35 +0900

    시간변경

--- app/Models/Sns.php
+++ app/Models/Sns.php
@@ -2,12 +2,55 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Sns
+ * @package App\Models
+ */
 class Sns extends Model
 {
+    /**
+     * @var string
+     */
     protected $table = "cp_sns";
+    /**
+     * @var array
+     */
     protected $hidden = ['updated_at', 'created_at'];
+    /**
+     * @var array
+     */
     protected $guarded = [];
 
+
+    /**
+     * @param $value
+     * @return string
+     */
+    public function getTextCreatedAtAttribute($value)
+    {
+        return $this->dateTimeViewer(Carbon::parse($value)->timestamp);
+    }
+
+    /**
+     * @param $time
+     * @return string
+     */
+    public function dateTimeViewer($time)
+    {
+        $timeLater = time() - $time;
+        if ($timeLater < 60) {
+            return "방금 전";
+        } elseif ($timeLater < 60*60) {
+            return floor($timeLater / 60)."분 전";
+        } elseif ($timeLater < 60*60*24) {
+            return floor($timeLater / (60*60))."시간 전";
+        } elseif ($timeLater < 60*60*24*30) {
+            return floor($timeLater / (60*60*24))."일 전";
+        } else {
+            return floor($timeLater / (60*60*24*30))."달 전";
+        }
+    }
 }
