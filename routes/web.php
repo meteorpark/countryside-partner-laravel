3c9e8c8b5addc0380e33ff10ebbccee679816256
rzip84 <rzip84@naver.com>
2019-07-05 10:00:28 +0900

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
